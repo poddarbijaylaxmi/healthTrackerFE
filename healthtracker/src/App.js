f3680dcd660639b84e447f1e26fddc04b7c76969
Bijay Laxmi <bijay.laxmi@adventz.com>
2025-01-27 14:55:14 +0530

    Routing added

--- healthtracker/src/App.js
+++ healthtracker/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
-  return <div className="text-blue-600">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage/>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
